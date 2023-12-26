3fcbd59576011d17ef86af779958cd745422f2a1
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-26 15:43:34 +0400

    Update loqika.java

--- loqika.java
+++ loqika.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
- 
+
 public class loqika {
- 
-    public static void main(String[] args) {
- 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Vvedite chislo: ");
-        int a = in.nextInt();
- 
-        if(a > 2 && a < 9){
-            System.out.println("chislo bolshe 2 i menshe 9");
-        }
-        else{
-            System.out.println("Chislo ne pravilno vvedeno");
-        }
-       
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    System.out.println("Vvedite chislo: ");
+    int a = in.nextInt();
+
+    if (a > 2 && a < 9) {
+      System.out.println("chislo bolshe 2 i menshe 9");
+    } else {
+      System.out.println("Chislo ne pravilno vvedeno");
     }
+
+  }
 }
