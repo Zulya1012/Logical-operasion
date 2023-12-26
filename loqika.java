7ec5140cb8e86732c751cfead4b1a5f66d917d32
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-26 15:46:09 +0400

    Update loqika.java

--- loqika.java
+++ loqika.java
@@ -5,13 +5,13 @@ public class loqika {
   public static void main(String[] args) {
 
     Scanner in = new Scanner(System.in);
-    System.out.println("Vvedite chislo: ");
+    System.out.println("Enter the number: ");
     int a = in.nextInt();
 
     if (a > 2 && a < 9) {
-      System.out.println("chislo bolshe 2 i menshe 9");
+      System.out.println("Number greater than 2 and less than 9");
     } else {
-      System.out.println("Chislo ne pravilno vvedeno");
+      System.out.println("The number was entered incorrectly");
     }
 
   }
